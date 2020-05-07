04b62c2b81158b388f3a64ca4296b683b0635800
Viviana Menzel <vivianamenzel@dr-menzel-it.de>
2020-05-07 12:06:06 +0200

    Code improvement after CodeSniffer

--- contactarticlesimage.php
+++ contactarticlesimage.php
@@ -1,9 +1,21 @@
 <?php
+/**
+ * Contact Articles Image Plugin
+ *
+ * @copyright  Copyright (C) 2020 Viviana Menzel. All rights reserved.
+ * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
+ */
+
 defined('_JEXEC') or die;
 
 use Joomla\CMS\Plugin\CMSPlugin;
 use Joomla\Utilities\ArrayHelper;
 
+/**
+ * Plugin adds intro-image to articles in contact
+ *
+ * @since  1.0
+ */
 class PlgContentContactArticlesImage extends CMSPlugin
 {
 	/**
@@ -18,7 +30,7 @@ class PlgContentContactArticlesImage extends CMSPlugin
 	 * Plugin that retrieves contact information for contact
 	 *
 	 * @param   string   $context  The context of the content being passed to the plugin.
-	 * @param   mixed    &$row     An object with a "text" property
+	 * @param   mixed    $row      An object with a "text" property
 	 * @param   mixed    $params   Additional parameters. See {@see PlgContentContent()}.
 	 * @param   integer  $page     Optional page number. Unused. Defaults to zero.
 	 *
@@ -26,35 +38,36 @@ class PlgContentContactArticlesImage extends CMSPlugin
 	 */
 	public function onContentPrepare($context, &$row, $params, $page = 0)
 	{
-		$allowed_contexts = array('com_contact.contact');
 
-		if (!in_array($context, $allowed_contexts) || empty($row->id))
+		if ($context != 'com_contact.contact' || empty($row->id))
 		{
 			return true;
 		}
 
 		$articleids 				= ArrayHelper::getColumn($row->articles, 'id');
 		$articleimages       		= $this->getArticleImage($articleids);
+
 		foreach ($row->articles as $article)
 		{
 			if (!isset($articleimages[$article->id]))
 			{
-				continue; 
+				continue;
 			}
-		  
+
 			$images  					= json_decode($articleimages[$article->id]->images);
-			$intro_image 				= $images->image_intro;
-			$intro_image_alt 			= $images->image_intro_alt;
-			$article->introimage		= $intro_image; 
-			$article->introimagealt		= $intro_image_alt; 
+			$introImage 				= $images->image_intro;
+			$introImageAlt	 			= $images->image_intro_alt;
+			$article->introimage		= $introImage;
+			$article->introimagealt		= $introImageAlt;
 		}
+
 		return true;
 	}
 
 	/**
 	 * Retrieve intro image from article
 	 *
-	 * @param   int  $articleide Id of the article
+	 * @param   array  $articleids Id of the articles
 	 *
 	 * @return  mixed|null|integer
 	 */
@@ -65,14 +78,14 @@ class PlgContentContactArticlesImage extends CMSPlugin
 
 		if (empty($articleids))
 		{
-			return array(); 
+			return array();
 		}
-		
+
 		$query = $this->db->getQuery(true);
-	  
-		$query	->select($this->db->quoteName(['id', 'images']))
-				->from($this->db->quoteName('#__content'))
-				->whereIn($this->db->quoteName('id'), $articleids);
+
+		$query->select($this->db->quoteName(array('id', 'images')))
+			->from($this->db->quoteName('#__content'))
+			->whereIn($this->db->quoteName('id'), $articleids);
 
 		$articleimages = $this->db->setQuery($query)->loadObjectList('id');
 
